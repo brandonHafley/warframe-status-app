a1e1ccffcd6a938f2865d38d64072ebfd05ed7f1
brandonHafley <bchafley@gmail.com>
2022-12-23 13:25:25 -0600

    Initial commit for push to GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [statusData, setStatusData] = useState([]);
+
+    const getStatusData = fetch("https://api.warframestat.us/pc/fissures?language=en", {
+        method: 'GET',
+        redirect: 'follow'
+    })
+        .then(response => response.json())
+        .then(result => result)
+        .catch(error => console.log('error', error));
+
+    const parseStatusData = async () => {
+        setStatusData(await getStatusData);
+    }
+
+    useEffect(() => {
+        parseStatusData();
+    }, []);
+
+    return (
+        <div className="App">
+            <header>
+                <h1>Warframe Status App</h1>
+                {statusData.map(el => (
+                    <div key={el.id}>
+                        {el.enemy}
+                        {el.missionType}
+                        {el.node}
+                    </div>
+                ))}
+            </header>
+        </div>
+    );
 }
 
 export default App;
